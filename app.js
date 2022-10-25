53f7413bd1080c64882ff9503754e3a26bbad4b4
LEAH WILNER <leahwilner1@gmail.com>
2022-10-24 20:33:37 -0500

    add whileloop for number input

--- app.js
+++ app.js
@@ -8,38 +8,68 @@ function getUserName(){
 //codde block within the {}
 userName = prompt('What is your name?');
 console.log('What is our input for user name: '   , userName);
-let nameConfirm = confirm('Is the really your name?');
-console.log('Did they confirm? ', nameConfirm);
+// let nameConfirm = confirm('Is the really your name?');
+// console.log('Did they confirm? ', nameConfirm);
 }
 
+function getUserTime(){
+ 
+  let time = parseInt(prompt('Give me your time please'));
+  // console.log('I need a number'. typeof(time))
+ 
+
+  while(isNaN(time)){
+    alert("Must input numbers");
+    let time = parseInt(prompt('Give me your time please'));
+  }
+
+  if(time <= 11){
+    message = 'Good Morning';
+  }
+
+  // if(time <= 11){
+  //   message = 'Good Morning';
+  // }else if(time >= 12 && time <= 18){
+  //   message = 'Good Afternoon';
+  // } else if(time > 18 && time < 24) {
+  //   message = 'Good Evening';
+  // } else {
+  //   message = 'hello, I dont know your time.';
+  // }
+  // return message;
+}
 
 
 
 
 
 // function keyword  and the function name, then () and {}
-function getUserTime(){
-  let time = prompt('What hour is it in Military time?');
-  console.log('User time is: ', time);
-  if(time <= 11){
-    message = 'Good Morning';
-  } else if(time > 12 && time <= 17){
-    message = 'Good Afternoon';
-  } else if(time > 18 && time < 24){
-    message = 'Good Evening';
-  } else {
-    message = 'Hello, I dont know your time.';
-  }
-  console.log('our message from if else condition is: ', message);
-}
+// function getUserTime(){
+//   let time = prompt('What hour is it in Military time?');
+//   console.log('User time is: ', time);
+//   if(time <= 11){
+//     message = 'Good Morning';
+//   } else if(time > 12 && time <= 17){
+//     message = 'Good Afternoon';
+//   } else if(time > 18 && time < 24){
+//     message = 'Good Evening';
+//   } else {
+//     message = 'Hello, I dont know your time.';
+//   }
+//   console.log('our message from if else condition is: ', message);
+// }
 
 //invoke our functions we call the function to run it. 
+
+
+
+
+
+
+
 getUserName();
 getUserTime();
 
 
 
 document.write('Hello ' + userName + '! ' + message);
-
-
-
